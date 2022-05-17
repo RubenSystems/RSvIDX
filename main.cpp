bd7273bf280fe88b47fda74218efcdd9a0be806f
RubenSystems <ruben.t.j@icloud.com>
2022-05-17 08:31:41 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RSvIDX
+//
+//  Created by Ruben Ticehurst-James on 17/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
