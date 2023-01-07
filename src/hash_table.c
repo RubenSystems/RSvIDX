86701238b4c3bac4780adc539b850086861aba82
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2023-01-07 17:34:01 +0000

    Made hashtable forward declared

--- src/hash_table.c
+++ src/hash_table.c
@@ -7,3 +7,78 @@
 
 #include "hash_table.h"
 
+
+struct hash_table init_hash_table() {
+	struct hash_table _init_val = {
+		.buckets = calloc(HT_INITIAL_SIZE, sizeof(struct hash_bucket)),
+		.used = 0,
+		.allocated = HT_INITIAL_SIZE
+	};
+	return _init_val;
+}
+
+void hash_table_add(struct hash_table * table, size_t key, size_t value) {
+	size_t mask = table->allocated - 1;
+	size_t index = key & mask;
+	size_t probe = key;
+	
+	while (true) {
+		if (table->buckets[index].status == BUCKET_BRIDGE ||
+			table->buckets[index].status == BUCKET_EMPTY) {
+			
+			struct hash_bucket _new_bucket = {
+				.status = BUCKET_OCCUPIED,
+				.key = key,
+				.value = value
+			};
+			
+			table->buckets[index] = _new_bucket;
+			return;
+		} else if(table->buckets[index].key == key) {
+			table->buckets[index].value = value;
+		}
+		
+		probe >>= HT_PROBE_SHIFT;
+		index = mask & (index * 5 + probe + 1);
+	}
+}
+
+enum bucket_operation_response hash_table_get(struct hash_table * table, size_t key, struct hash_bucket * bucket) {
+	size_t mask = table->allocated - 1;
+	size_t index = key & mask;
+	size_t probe = key;
+	
+	while (true) {
+		if (table->buckets[index].key == key &&
+			table->buckets[index].status == BUCKET_OCCUPIED) {
+			
+			*bucket = table->buckets[index];
+			return BUCKET_EXISTS;
+		} else if (table->buckets[index].status == BUCKET_EMPTY) {
+			return BUCKET_DOES_NOT_EXIST;
+		}
+		
+		probe >>= HT_PROBE_SHIFT;
+		index = mask & (index * 5 + probe + 1);
+	}
+}
+
+
+enum bucket_operation_response hash_table_delete(struct hash_table * table, size_t key) {
+	size_t mask = table->allocated - 1;
+	size_t index = key & mask;
+	size_t probe = key;
+	
+	while (true) {
+		if (table->buckets[index].key == key &&
+			table->buckets[index].status == BUCKET_OCCUPIED) {
+			table->buckets[index].status = BUCKET_BRIDGE;
+			return BUCKET_EXISTS;
+		} else if (table->buckets[index].status == BUCKET_EMPTY) {
+			return BUCKET_DOES_NOT_EXIST;
+		}
+		
+		probe >>= HT_PROBE_SHIFT;
+		index = mask & (index * 5 + probe + 1);
+	}
+}
--- src/include/hash_table.h
+++ src/include/hash_table.h
@@ -43,79 +43,12 @@ struct hash_table {
 	size_t 					allocated;
 };
 
-static struct hash_table init_hash_table(){
-	struct hash_table _init_val = {
-		.buckets = calloc(HT_INITIAL_SIZE, sizeof(struct hash_bucket)),
-		.used = 0,
-		.allocated = HT_INITIAL_SIZE
-	};
-	return _init_val;
-}
+struct hash_table init_hash_table(void);
 
-static void hash_table_add(struct hash_table * table, size_t key, size_t value) {
-	size_t mask = table->allocated - 1;
-	size_t index = key & mask;
-	size_t probe = key;
-	
-	while (true) {
-		if (table->buckets[index].status == BUCKET_BRIDGE ||
-			table->buckets[index].status == BUCKET_EMPTY) {
-			
-			struct hash_bucket _new_bucket = {
-				.status = BUCKET_OCCUPIED,
-				.key = key,
-				.value = value
-			};
-			
-			table->buckets[index] = _new_bucket;
-			return;
-		} else if(table->buckets[index].key == key) {
-			table->buckets[index].value = value;
-		}
-		
-		probe >>= HT_PROBE_SHIFT;
-		index = mask & (index * 5 + probe + 1);
-	}
-}
+void hash_table_add(struct hash_table * table, size_t key, size_t value) ;
 
-static enum bucket_operation_response hash_table_get(struct hash_table * table, size_t key, struct hash_bucket * bucket) {
-	size_t mask = table->allocated - 1;
-	size_t index = key & mask;
-	size_t probe = key;
-	
-	while (true) {
-		if (table->buckets[index].key == key &&
-			table->buckets[index].status == BUCKET_OCCUPIED) {
-			
-			*bucket = table->buckets[index];
-			return BUCKET_EXISTS;
-		} else if (table->buckets[index].status == BUCKET_EMPTY) {
-			return BUCKET_DOES_NOT_EXIST;
-		}
-		
-		probe >>= HT_PROBE_SHIFT;
-		index = mask & (index * 5 + probe + 1);
-	}
-}
+enum bucket_operation_response hash_table_get(struct hash_table * table, size_t key, struct hash_bucket * bucket);
 
-
-static enum bucket_operation_response hash_table_delete(struct hash_table * table, size_t key) {
-	size_t mask = table->allocated - 1;
-	size_t index = key & mask;
-	size_t probe = key;
-	
-	while (true) {
-		if (table->buckets[index].key == key &&
-			table->buckets[index].status == BUCKET_OCCUPIED) {
-			table->buckets[index].status = BUCKET_BRIDGE;
-			return BUCKET_EXISTS;
-		} else if (table->buckets[index].status == BUCKET_EMPTY) {
-			return BUCKET_DOES_NOT_EXIST;
-		}
-		
-		probe >>= HT_PROBE_SHIFT;
-		index = mask & (index * 5 + probe + 1);
-	}
-}
+enum bucket_operation_response hash_table_delete(struct hash_table * table, size_t key);
 
 #endif /* hash_table_h */
